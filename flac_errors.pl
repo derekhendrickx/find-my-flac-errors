01b21f055bd37c843b59bc62b38bbfb977ea9ef4
Derek Hendrickx <derek@hendrickx.me>
2016-12-28 11:38:12 +0100

    Initial commit

---
+++ flac_errors.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Path::Class;
+use autodie; # die if problem reading or writing a file
+
+my $dir = dir("."); # /tmp
+
+my $file = $dir->file("music-errors.txt");
+
+# Read in the entire contents of a file
+my $content = $file->slurp();
+
+# openr() returns an IO::File object to read from
+my $file_handle = $file->openr();
+
+# Read in line at a time
+my $nbError = 0;
+while (my $line = $file_handle->getline()) {
+	if ($line =~ m/Error converting/) {
+		$nbError++;
+	}
+}
+
+print "$nbError errors\n";
