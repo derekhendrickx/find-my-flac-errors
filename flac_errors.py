74983020db5cfcd3e81e258837979522f2d1b639
Derek Hendrickx <derek@hendrickx.me>
2016-12-31 18:37:17 +0100

    Use a Set to avoid duplicates

--- flac_errors.py
+++ flac_errors.py
@@ -2,6 +2,7 @@ import re
 
 errorFile = open('samples/foobar2000-errors.txt', 'r')
 errors = errorFile.readlines()
+errorsSet = set()
 
 index = -1
 for i in range(len(errors)):
@@ -12,10 +13,11 @@ for i in range(len(errors)):
 nbErrorsFoobar = 0
 for i in range(index, len(errors)):
 	line = errors[i]
-	match = re.search(r'"(.*.flac)"', line)
+	match = re.search(r'\\.+\\([^\\]+.flac)', line)
 	if match:
+		errorsSet.add(match.group(1))
 		nbErrorsFoobar += 1
-		print(match.group(1))
+		# print(match.group(1))
 
 errorFile.close()
 
@@ -26,11 +28,16 @@ index = -1
 nbErrorsDbPowerAmp = 0
 for i in range(len(errors)):
 	line = errors[i]
-	match = re.search(r'/.+/[^/]+.flac', line)
+	match = re.search(r'/.+/([^/]+.flac)', line)
 	if match:
-		print(match.group())
+		errorsSet.add(match.group(1))
+		# print(match.group(1))
 	if (re.search(r'Encountered', line) or re.search(r'md5 did not match decoded data, file is corrupt.', line)):
 		nbErrorsDbPowerAmp += 1
 
 print(nbErrorsFoobar, 'FLAC errors from foobar2000')
 print(nbErrorsDbPowerAmp, 'FLAC errors from dbPowerAmp')
+print(len(errorsSet))
+
+for item in errorsSet:
+	print(item)
