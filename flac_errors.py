48fc3dcace8cc19c208b78836231c337853373ae
Derek Hendrickx <derek@hendrickx.me>
2017-01-04 18:06:32 +0100

    Refactor as a class

--- flac_errors.py
+++ flac_errors.py
@@ -8,83 +8,101 @@ import re
 
 import trackWithError
 
-def main():
-	errorFile = open('samples/foobar2000-errors.txt', 'r')
-	errors = errorFile.readlines()
-	errorsDict = {}
+class FindFLACErrors:
+	def __init__(self):
+		self.errorsDict = {}
+		self.nbErrors = [0, 0]
 
-	index = -1
-	for i in range(len(errors)):
-		line = errors[i]
-		if re.search(r'List of undecodable items:', line):
-			index = i
-			break
+	def findErrors(self, program, files):
+		options = {
+			'foobar': self.findErrorsFromFoobar,
+			'dbpoweramp': self.findErrorsFromDbPowerAmp
+		}
+		options.get(program)(files)
 
-	nbErrorsFoobar = 0
-	for i in range(index, len(errors)):
-		line = errors[i]
-		match = re.search(r'[^".]+\\.+\\([^\\]+.flac)', line)
-		if match:
-			trackName = match.group(1)
-			normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
-			error = trackWithError.TrackWithError(trackName, match.group())
-			if normalizedTrackName in errorsDict:
-				error = errorsDict.get(normalizedTrackName)
-				error.addCount()
-			errorsDict[normalizedTrackName] = error
-			nbErrorsFoobar += 1
+	def findErrorsFromFoobar(self, files):
+		errorFile = open(files[0], 'r')
+		errors = errorFile.readlines()	
 
-	errorFile.close()
+		index = -1
+		for i in range(len(errors)):
+			line = errors[i]
+			if re.search(r'List of undecodable items:', line):
+				index = i
+				break
 
-	errorFile = open('samples/dbpoweramp-errors.txt', 'r')
-	errors = errorFile.readlines()
+		for i in range(index, len(errors)):
+			line = errors[i]
+			match = re.search(r'[^".]+\\.+\\([^\\]+.flac)', line)
+			if match:
+				trackName = match.group(1)
+				normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
+				error = trackWithError.TrackWithError(trackName, match.group())
+				if normalizedTrackName in self.errorsDict:
+					error = self.errorsDict.get(normalizedTrackName)
+					error.addCount()
+				self.errorsDict[normalizedTrackName] = error
+				self.nbErrors[0] += 1
 
-	nbErrorsDbPowerAmp = 0
-	for i in range(len(errors)):
-		line = errors[i]
-		if re.search(r'^\s{3}', line) == None:
+		errorFile.close()
+
+	def findErrorsFromDbPowerAmp(self, files):
+		errorFile = open(files[0], 'r')
+		errors = errorFile.readlines()
+
+		for i in range(len(errors)):
+			line = errors[i]
+			if re.search(r'^\s{3}', line) == None:
+				match = re.search(r'\/.+\/([^\/]+.flac)', line)
+			if match and (re.search(r'Encountered', line) or re.search(r'md5 did not match decoded data, file is corrupt.', line) or re.search(r'indicated sample count does not match decoded sample count, possible file corruption', line)):
+				trackName = match.group(1)
+				normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
+				error = trackWithError.TrackWithError(trackName, match.group())
+				if normalizedTrackName in self.errorsDict:
+					error = self.errorsDict.get(normalizedTrackName)
+					if error.count == 1:
+						self.nbErrors[1] += 1
+					error.addCount()
+				self.errorsDict[normalizedTrackName] = error
+
+		errorFile.close()
+
+		errorFile = open(files[1], 'r')
+		errors = errorFile.readlines()
+
+		for i in range(0, len(errors), 3):
+			line = errors[i]
 			match = re.search(r'\/.+\/([^\/]+.flac)', line)
-		if match and (re.search(r'Encountered', line) or re.search(r'md5 did not match decoded data, file is corrupt.', line) or re.search(r'indicated sample count does not match decoded sample count, possible file corruption', line)):
-			trackName = match.group(1)
-			normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
-			error = trackWithError.TrackWithError(trackName, match.group())
-			if normalizedTrackName in errorsDict:
-				error = errorsDict.get(normalizedTrackName)
-				if error.count == 1:
-					nbErrorsDbPowerAmp += 1
-				error.addCount()
-			errorsDict[normalizedTrackName] = error
+			if match:
+				trackName = match.group(1)
+				normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
+				error = trackWithError.TrackWithError(trackName, match.group())
+				if normalizedTrackName in self.errorsDict:
+					error = self.errorsDict.get(normalizedTrackName)
+					if error.count == 1:
+						self.nbErrors[1] += 1
+					error.addCount()
+				self.errorsDict[normalizedTrackName] = error
 
-	errorFile.close()
+		errorFile.close()
 
-	errorFile = open('samples/dbpoweramp-infos.txt', 'r')
-	errors = errorFile.readlines()
+	def writeResults(self, file):
+		print(self.nbErrors[0], 'FLAC errors from foobar2000')
+		print(self.nbErrors[1], 'FLAC errors from dbPowerAmp')
+		print('Total', len(self.errorsDict), 'errors')
 
-	for i in range(0, len(errors), 3):
-		line = errors[i]
-		match = re.search(r'\/.+\/([^\/]+.flac)', line)
-		if match:
-			trackName = match.group(1)
-			normalizedTrackName = trackWithError.TrackWithError.normalizeName(trackName)
-			error = trackWithError.TrackWithError(trackName, match.group())
-			if normalizedTrackName in errorsDict:
-				error = errorsDict.get(normalizedTrackName)
-				if error.count == 1:
-					nbErrorsDbPowerAmp += 1
-				error.addCount()
-			errorsDict[normalizedTrackName] = error
+		resultFile = open(file, 'w')
+		resultFile.write('Track name;Path;Count;\n')
+		i = 0
+		for k, v in self.errorsDict.items():
+			errorFile.write(v.name + ';' + v.path + ';' + str(v.count) + ';\n')
+		
+		resultFile.close()
 
-	errorFile.close()
-
-	print(nbErrorsFoobar, 'FLAC errors from foobar2000')
-	print(nbErrorsDbPowerAmp, 'FLAC errors from dbPowerAmp')
-	print('Total', len(errorsDict), 'errors')
-
-	errorFile = open('samples/results.txt', 'w')
-	errorFile.write('Track name;Path;Count;\n')
-	i = 0
-	for k, v in errorsDict.items():
-		errorFile.write(v.name + ';' + v.path + ';' + str(v.count) + ';\n')
+	def main(self):
+		self.findErrors('foobar', ['samples/foobar2000-errors.txt'])
+		self.findErrors('dbpoweramp', ['samples/dbpoweramp-errors.txt', 'samples/dbpoweramp-infos.txt'])
+		self.writeResults('samples/results.txt')
 
 if __name__ == "__main__":
-	main()
+	FindFLACErrors().main()
